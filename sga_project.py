073a3f706dfb28e7659a9ffb2cc3263cb2b6d399
Lehiem Seyoum <lehiem.s1@gmail.com>
2025-04-10 08:41:15 -0400

    repo update

---
+++ sga_project.py
@@ -0,0 +1,36 @@
+class Student(models.Model):
+    #student_id = models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    graduation_year = models.IntegerField()
+    student_email = models.EmailField()
+
+
+class Attendance(models.Model):
+    #attendance_id = models.IntegerField()
+    date = models.DateTimeField() 
+    attendance_status = models.BooleanField()
+    student_id = models.ForeignKey(student_id, on_delete=models.CASCADE) 
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=30)
+    event_start = models.DateTimeField() 
+    event_end = models.DateTimeField() 
+    event_coordinator = models.CharField(max_length=30)
+    approval = models.BooleanField()
+
+class Admin(models.Model):
+    admin_email = models.EmailField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    job_title = models.CharField(max_length=30)
+    room_number = models.IntegerField()
+
+class Event_approver(models.Model):
+    event_name = models.CharField(max_length=30)
+    admin_email = models.EmailField()
+    individual_approval_status = models.BooleanField()
+    # pk = models.CompositePrimaryKey("event_name", "admin_email")
+    event_name = models.ForeignKey(event_name, on_delete=models.CASCADE) 
+    admin_email = models.ForeignKey(admin_email, on_delete=models.CASCADE) 
