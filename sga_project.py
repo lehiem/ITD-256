e4546904c169a414a45156c3852fab5ea3b695af
Lehiem Seyoum <lehiem.s1@gmail.com>
2025-04-10 14:35:32 -0400

    final Q3 project

--- sga_project.py
+++ sga_project.py
@@ -1,3 +1,19 @@
+import sys
+
+try:
+    from django.db import models
+except Exception:
+    print('Exception: Django Not Found, please install it with "pip install django".')
+    sys.exit()
+
+
+# Sample User model
+class User(models.Model):
+    name = models.CharField(max_length=50, default="Dan")
+
+    def __str__(self):
+        return self.name
+
 class Student(models.Model):
     #student_id = models.IntegerField()
     first_name = models.CharField(max_length=30)
